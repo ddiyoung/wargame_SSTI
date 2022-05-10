f9e564111daa7aaf7542036aae89fa479e744e8c
ddiyoung <wsw3998@sju.ac.kr>
2022-05-10 13:57:24 +0900

    emailcheck function

--- SSTI/static/js/submit.js
+++ SSTI/static/js/submit.js
@@ -1,13 +1,28 @@
+function email_check(email) {
+
+	const reg = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+	return reg.test(email);
+
+}
+
 function submit(){
     const csrf_token = $('[name=csrfmiddlewaretoken]').val();
 
-    $.ajax({
+    const email = $('#email').val();
+
+    if(email_check(email)) {
+        $.ajax({
         url: './Newsletter',
         type: 'post',
         data: {
-            username: $('#email').val(),
+            email: $('#email').val(),
         },
         headers: {'X-CSRFToken': csrf_token},
         dataType: 'json',
-    })
+        });
+    }
+    else{
+        alert("이메일 형식을 확인해주세요!");
+    }
 }
